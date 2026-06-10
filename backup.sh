9a4ca6f0260a54698bd5a0af10b58b8f50d05004
opengaoling <opengaoling@users.noreply.github.com>
2026-06-10 06:02:12 +0000

    Exclude GeoIP databases from backups

--- backup.sh
+++ backup.sh
@@ -34,7 +34,12 @@ git clone "https://${GH_PAT}@github.com/${GH_BACKUP_USER}/${GH_REPO}.git" --dept
 if [ -d "/tmp/$GH_REPO" ]; then
     TIME=$(date "+%Y-%m-%d-%H:%M:%S")
     echo "↓↓↓↓↓↓↓↓↓↓ dashboard-$TIME.tar.gz list ↓↓↓↓↓↓↓↓↓↓"
-    tar czvf "/tmp/$GH_REPO/dashboard-$TIME.tar.gz" --exclude="data/tsdb" -C "$WORK_DIR" data/
+    tar czvf "/tmp/$GH_REPO/dashboard-$TIME.tar.gz" \
+        --exclude="data/tsdb" \
+        --exclude="data/geoip.db" \
+        --exclude="data/asn.mmdb" \
+        --exclude="data/GeoLite2-ASN.mmdb" \
+        -C "$WORK_DIR" data/
     echo -e "↑↑↑↑↑↑↑↑↑↑ dashboard-$TIME.tar.gz list ↑↑↑↑↑↑↑↑↑↑\n"
 
     # 更新备份 GitHub 库
